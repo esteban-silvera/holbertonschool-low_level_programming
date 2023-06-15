ac29164fd665106370a77a1dc9b456bfb24b6ce2
esteban-silvera <estebansilvera000@gmail.com>
2023-06-15 06:51:17 -0700

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - print"this print te text and return 0"
+ *Return: always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
