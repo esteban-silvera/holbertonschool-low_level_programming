cc7011e49455d13a356a6ee541edfe8ffc680ba0
esteban-silvera <estebansilvera000@gmail.com>
2023-06-15 05:36:45 -0700

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ *main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line."
+ *                return: always 0 
+ */
+
+int main(void)
+{
+	puts("\"Programing is like building a multilingua1 puzzle")
+
+		return (0);
+}
