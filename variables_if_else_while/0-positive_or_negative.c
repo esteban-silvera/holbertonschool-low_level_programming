99dfd4713f4ead73469d87f5a91341eed04ca477
esteban-silvera <estebansilvera000@gmail.com>
2023-06-15 10:25:41 -0700

    0 3

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,7 @@
 /**
  *main - prints "the code generate a one number random to N and comparated whit
  0 for now is a positive negativo or 0"
+ *Return: Always 0.
  */
 
 int main(void)
@@ -12,18 +13,17 @@ int main(void)
 	srand(time(NULL));
 	int n = rand();
 
-	if (n > 0);
+	if (n > 0)
 	{
-		printf("is positive");
+		printf("is positive.\n", n);
 	}
 	else if (n < 0)
 	{
-		prifnt("is negative");
+		prifnt("is negative.\n", n);
 	}
 	else
 	{
-	       	prinft("is zero");
+		prinft("is zero.\n", n);
 	}
-	return 0;
+	return (0);
 }
-
