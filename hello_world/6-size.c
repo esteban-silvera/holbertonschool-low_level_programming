7b5e56ec4dcab77b2695803d9e4b99aface3bba5
esteban-silvera <estebansilvera000@gmail.com>
2023-06-15 08:29:02 -0700

    6 5

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,11 +10,14 @@ int main(void)
 {
 	int intType;
 	float floatType;
-	long int LongIntType;
+	long int longintType;
 	char charType;
+	long long int; longlongType
+
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(LongIntType));
+	printf("Size of long int: %zu bytes\n", sizeof(LongIntType));
 	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
 	return (0);
 }
