f102ed9a27d3fa47f250ff1d615d2a9018ff35dd
esteban-silvera <estebansilvera000@gmail.com>
2023-06-15 07:22:03 -0700

    6 1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,9 +11,10 @@ int main(void)
 	float floatType;
 	double doubleType;
 	char charType;
+	
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
 	printf("Size of double: %zu bytes\n", sizeof(doubleType));
 	printf("Size of char: %zu byte\n", sizeof(charType));
-	return 0;
+	return (0);
 }
