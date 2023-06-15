a682adbb7adb6dc1442a765e312fd754da5e041d
esteban-silvera <estebansilvera000@gmail.com>
2023-06-15 08:34:53 -0700

    6 8

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-	
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
@@ -16,5 +15,4 @@ int main(void)
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
-
 }
