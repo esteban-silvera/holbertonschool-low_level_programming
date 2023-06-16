d2d1c3a845280013a33b0eb9ad2b85ac76e62834
esteban-silvera <estebansilvera000@gmail.com>
2023-06-16 09:49:24 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 10 ; number++)
+	{
+		putchar(number);
+	}
+
+	putchar('\n');
+	return (0);
+}
