1cb3524f1898f8ac5205d419d9132acc6b6d1dd1
esteban-silvera <estebansilvera000@gmail.com>
2023-06-16 10:41:52 -0700

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Main Function "print te abcdario"
+ * Return: 0.
+ */
+int main(void)
+{
+	char letras;
+
+	for (letras = 'z'; letras <= 'a'; letras--)
+	{
+		putchar(letras);
+	}
+	putchar('\n');
+	return (0);
+}
