07b5352196f1dca44ab37123f55085c6207eea26
esteban-silvera <estebansilvera000@gmail.com>
2023-06-16 08:33:26 -0700

    2 5

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - Main Function "print te abcdario"
+ * Return: 0.
+ */
 int main(void)
 {
 	char letras;
-	for (letras = 'a'; letras <= 'z'; letras++){
+
+	for (letras = 'a'; letras <= 'z'; letras++)
+	{
 		putchar(letras);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
