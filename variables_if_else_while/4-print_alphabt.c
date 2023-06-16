312bc365754f8dd883020b7fb6d9629ab4341057
esteban-silvera <estebansilvera000@gmail.com>
2023-06-16 09:28:27 -0700

    4 1

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0.
+ */
+
+int main() 
+{
+	char NoQE;
+	
+	for (NoQE = 'a'; NoQE <= 'z'; NoQE++)
+	{
+		if (NoQE != "e" || "q")
+		{
+			putchar(NoQE):
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
