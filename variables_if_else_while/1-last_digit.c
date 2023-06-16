1cb8336581f5816068fd0e0d7b2133a461b87a55
esteban-silvera <estebansilvera000@gmail.com>
2023-06-16 06:58:32 -0700

    1 9

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,21 +2,30 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Print "in this scripts used a comparated whit random number
+ * for now is more of 5 equal to 0 or less to 6"
+ * Return: Always 0.
+ */
 
 int main(void)
 {
 	int n;
 	int un;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	un = n % 10;
-	if (un > 5){
-		printf("Last digit of %d is %d and is greater than 5\n",n ,un);
-	}else if (un == 0){
-		printf("Last digit of %d is %d and is 0\n",n ,un);
-	}else if (un < 6){
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",n ,un);
+	if (un > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, un);
+	} else if (un == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, un);
+	} else if (un < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, un);
 	}
 	return (0);
 }
