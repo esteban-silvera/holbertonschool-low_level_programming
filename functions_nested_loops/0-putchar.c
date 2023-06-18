7c53b2e26ac6640778c4d5bb6d1fa55ac10bb5c1
esteban-silvera <estebansilvera000@gmail.com>
2023-06-18 12:12:41 -0700

    0 4

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,12 +8,13 @@
 
 int main(void)
 {
-	const char* strg = "_putchar";
+	const char strg = "_putchar";
 	int i;
 
 	for (i = 0; strg[i] != '\0'; i++)
 	{
 		putchar(strg[i]);
 	}
+	putchar('\n');
 	return (0);
 }
