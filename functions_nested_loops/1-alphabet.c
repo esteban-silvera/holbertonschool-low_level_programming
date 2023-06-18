9559bd6af94530c6c47c9300441e3e075ab1413f
esteban-silvera <estebansilvera000@gmail.com>
2023-06-18 12:20:22 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_alphabet- Entry point
+ *Return: Always 0 (Success)
+ */
+int  main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
