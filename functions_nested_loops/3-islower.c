de68456eb96e5d809e3757a053a1c6324fff25d3
esteban-silvera <estebansilvera000@gmail.com>
2023-06-18 16:54:39 -0700

    3 1

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *_islower - verific is lower or not 
+ *_islower - verific is lower or not
  *
- * Return: Returns 1 if c is lower or Returns 0 is not lower 
+ * Return: Returns 1 if c is lower or Returns 0 is not lower
  */
 	int _islower(int c)
 {
