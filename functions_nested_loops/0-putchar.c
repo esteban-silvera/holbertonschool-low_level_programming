0a4fd6893578fe154fa50ab3eaef3854d4fa4d46
esteban-silvera <estebansilvera000@gmail.com>
2023-06-18 11:28:36 -0700

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0.
+ */
+
+int main(void)
+{
+	const char* strg = "_putchar";
+	int i;
+
+	for (i = 0; strg[i] != '\0'; i++)
+	{
+		putchar(strg[i]);
+	}
+	return (0);
+}
