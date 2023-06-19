b3cbba8df55601ec8e5fdb28488ea9486ae01fca
esteban-silvera <estebansilvera000@gmail.com>
2023-06-19 06:07:44 -0700

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *Return: 
+ */
+
+void print_to_98(int n);
+{
+	int nu;
+
+	for (nu = n; nu <= 98; nu++)
+	{
+		printf("%d", nu)
+		if (nu < 98)
+		{
+
+			printf(",")
+			printf(" ")
+		}
+	}
+		if (n > 98)
+		{
+			for (nu = n; nu >= 98; nu--)
+				{
+					printf("%d")
+						if (nu > 98)
+						{
+							printf(",")
+							printf(" ")
+						}
+
+				}
+		}
+		printf("\n")
+}
