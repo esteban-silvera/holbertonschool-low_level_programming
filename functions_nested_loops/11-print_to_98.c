38e39f764986d34296445c4ab3bcad62282d2c40
esteban-silvera <estebansilvera000@gmail.com>
2023-06-19 06:18:06 -0700

    11 5

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,37 +2,36 @@
 #include <stdio.h>
 /**
  *print_to_98 - function imprit to number 98
- *@n; its a number recive
+ *@n: its a number recive
  *
- *Return: 
  */
 
 void print_to_98(int n)
 {
+
 	int nu;
 
 	for (nu = n; nu <= 98; nu++)
 	{
-		printf("%d", nu);
-		if (nu < 98)
-		{
+	printf("%d", nu);
+	if (nu < 98)
+	{
 
-			printf(",");
-			printf(" ");
-		}
+	printf(",");
+	printf(" ");
 	}
-		if (n > 98)
-		{
-			for (nu = n; nu >= 98; nu--)
-				{
-					printf("%d", nu);
-						if (nu > 98)
-						{
-							printf(",");
-							printf(" ");
-						}
-
-				}
-		}
+	}
+	if (n > 98)
+	{
+	for (nu = n; nu >= 98; nu--)
+	{
+	printf("%d", nu);
+	if (nu > 98)
+	{
+	printf(",");
+	printf(" ");
+	}
+	}
+	}
 		printf("\n");
 }
