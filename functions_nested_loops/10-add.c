6b7c01cf8d4f2cee9004ea5250ff4c5c16d17265
esteban-silvera <estebansilvera000@gmail.com>
2023-06-19 05:53:38 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - add recived two number
+ *@a:  a number
+ *@b: b number
+ *Return: a+b
+ */
+int add(int a, int b)
+{
+	int result;
+	result = a + b;
+	return (result);
+}
