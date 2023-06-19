902dc46c523d5641c888cce4751b1b1e1e9613ae
esteban-silvera <estebansilvera000@gmail.com>
2023-06-18 17:52:07 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_last_digit - sript pritnf
+ *@n: value recived
+ *Return: Always 1.
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10
+	_putchar(last + '0');
+	return (1);
+}
