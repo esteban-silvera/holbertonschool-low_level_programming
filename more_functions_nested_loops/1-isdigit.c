cecafa0d56761aa56ed498ac55ad356ba150fcf6
esteban-silvera <estebansilvera000@gmail.com>
2023-06-20 12:51:30 -0700

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - funcition verifi is a number or not
+ *@c: is a paramet
+ * Return: return 1 is a number 0 is dont a number
+ */
+
+int _isupper(int c)
+{
+        if (c >= '0' && c <= '9')
+        {
+        return (1);
+        }
+	else if (c >= 'a' && c <= 'z')
+	{
+	return (0);
+	}
+        return (0);
+}
