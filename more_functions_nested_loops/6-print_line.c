645aaa83d23336ba6540433ed385b0eb31cea2c6
esteban-silvera <estebansilvera000@gmail.com>
2023-06-20 14:49:43 -0700

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_line - function prints the line
+ *@n: value
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	for (line = 1; line <= n; line++)
+	{
+	_putchar('_');
+
+	}
+	_putchar('\n');
+}
