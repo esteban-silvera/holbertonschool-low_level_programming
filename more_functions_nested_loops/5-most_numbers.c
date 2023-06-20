71c851bf191cf1127ffa955679c25333bc6b334d
esteban-silvera <estebansilvera000@gmail.com>
2023-06-20 14:04:14 -0700

    5

---
+++ more_functions_nested_loops/5-most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *more_numbers- function prints so much numbers
+ */
+void more_numbers(void)
+{
+	int times;
+	int num;
+
+	for (times = '0'; times <= '9'; times++)
+	{
+	for (num = '0'; num <= '14'; num++)
+	{
+	if (x >= 10)
+	{
+	_putchar((num / 10) + '0');
+	}
+	_putchar((num % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
