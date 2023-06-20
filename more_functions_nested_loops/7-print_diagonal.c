697d2988870f33f9d8875e0c1b2e145a63e849cb
esteban-silvera <estebansilvera000@gmail.com>
2023-06-20 15:32:39 -0700

    7 2

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,11 +10,11 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-	_putchar('\n')
+	_putchar('\n');
 	}
 	else
 	{
-	for (dia = 0; dia <= n; +dia+)
+	for (dia = 0; dia <= n; dia++)
 	{
 	for (line = 0; line < dia; line++)
 	{
