67d5cf0d1f2e94c5f6bba67f1b62049d5aa6657d
esteban-silvera <estebansilvera000@gmail.com>
2023-06-20 14:09:02 -0700

    5 2

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 	int times;
 	int num;
 
-	for (times = '0'; times <= '9'; times++)
+	for (times = 0; times <= 9; times++)
 	{
-	for (num = '0'; num <= '14'; num++)
+	for (num = 0; num <= 14; num++)
 	{
 	if (num >= 10)
 	{
