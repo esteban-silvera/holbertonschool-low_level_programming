58fcf68061ef95f86ea7570487dcf93f18021a55
esteban-silvera <estebansilvera000@gmail.com>
2023-06-20 12:59:58 -0700

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplica los numeros a y b
+ * @a: first paramet
+ * @b: second paramet
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int resu;
+
+	resu = a * b;
+	return(resu);
+}
