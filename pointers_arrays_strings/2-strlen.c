62bb62fbc9f6068c15d7f3a45b3ad1e7d9c0d6cc
esteban-silvera <estebansilvera000@gmail.com>
2023-06-21 10:06:30 -0700

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sterlen - length of a string
+ * @s: is the string retung the length
+ */
+
+int _strlen(char *s)
+{
+	sterlen(*s)
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 
 #endif
