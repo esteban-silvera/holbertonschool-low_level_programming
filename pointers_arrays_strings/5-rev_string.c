fdbcef1a457c88db1d252176e14d82cacb85c918
esteban-silvera <estebansilvera000@gmail.com>
2023-06-22 15:47:16 -0700

    5 7

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,7 +8,6 @@ void rev_string(char *s)
 {
 	int i;
 	int x = 0;
-	int j,
 	char o;
 
 	while (s[x] != '\0')
@@ -21,10 +20,9 @@ void rev_string(char *s)
 	for (i = 0; i < x / 2; i++)
 	{
 		o = s[i];
-		s[i] = s[j]
-		s[j] = o
+		s[i] = s[j];
+		s[j] = o;
 		j--;
-
 	}
 
 
