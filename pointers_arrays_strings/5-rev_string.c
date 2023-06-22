69a3775ea09c71bcb58cc1f7b55a9bcb07bf5423
esteban-silvera <estebansilvera000@gmail.com>
2023-06-21 18:12:48 -0700

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_rev - function print a text reverse
+ *@s: the string
+ */
+
+void rev_string(char *s)
+{
+	int x;
+	int n = 0;
+	char ss;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+
+	for (x = n - 1; x >= 0; x--)
+	{
+		ss = s[x];
+	}
+}
