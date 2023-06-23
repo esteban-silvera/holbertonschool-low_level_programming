9aa2c655485418ce16d9dd69dcf8377cb57497b3
esteban-silvera <estebansilvera000@gmail.com>
2023-06-23 09:53:18 -0700

    00 1

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char x;
-	x = dest + src;
+	int n = 0;
+	int nu;
+
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+	for (nu = 0; src[nu] != '\0'; nu++; n++)
+	{
+		dest[n] = src[nu];
+	}
+	dest[n] = '\0';
+
+	return (dest);
+	
 }
