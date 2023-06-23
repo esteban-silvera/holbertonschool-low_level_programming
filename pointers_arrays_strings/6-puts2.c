a30da092a34fcb025b008b8a7fb2a2664e9280d0
esteban-silvera <estebansilvera000@gmail.com>
2023-06-22 17:09:23 -0700

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *puts2 - Function pritns a number
+ *@str: the value
+ */
+
+void puts2(char *str)
+{
+	int x = 0;
+	int n;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	for (n = 0; n < x; n++)
+	{
+		if (n / 2 == 0)
+			{
+				_putchar(str[n]);
+			}
+	}
+	_putchar('\n');
+}
