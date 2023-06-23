b052d2e9ec44359623658e5c1f3c2f3426cb1108
esteban-silvera <estebansilvera000@gmail.com>
2023-06-23 06:38:02 -0700

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - is the function
+ * @dest: destinatarion
+ * @src: lo que hay que copiar
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	while (src[x] != '\0')
+	{
+		dest[x] == src[x];
+		x++;
+	}
+
+	dest[x] = '\0'
+}
