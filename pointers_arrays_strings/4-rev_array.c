1946741abfc758a2a510b5ce6d18c91f222244af
esteban-silvera <estebansilvera000@gmail.com>
2023-06-26 05:35:48 -0700

    4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x;
+	int t;
+	int l;
+
+	for (t = n - 1; t >= n / 2; t--)
+	{
+		l = a
+		x = l[t]
+		a = x
+	}
+
+}
