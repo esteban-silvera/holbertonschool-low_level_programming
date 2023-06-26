1aa796ef813c05b5aca9f11576ea5b86d5496873
esteban-silvera <estebansilvera000@gmail.com>
2023-06-26 07:15:34 -0700

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+char *cap_string(char *str)
+{
+	int x = 0;
+
+	while (str[x])
+	{
+		if (str[x] != ',' || ';' ||'.' || '!' || '?' || '"' || '(' || ')' || '{' || '}')
+		{
+			x++
+		}
+		else (x++; x += 32)
+	}
+
+}
