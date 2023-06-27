a40bda9e2125e9d93771d55784cbfb70c324e49b
esteban-silvera <estebansilvera000@gmail.com>
2023-06-27 15:59:07 -0700

    7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
-/** 
- * print_chessboard -  the fuction print a cheasboard
+/**
+ * print_chessboard - the fuction print a cheasboard
  * @a: the fichas
- * @8: the logn of a board
  */
 
 void print_chessboard(char (*a)[8])
