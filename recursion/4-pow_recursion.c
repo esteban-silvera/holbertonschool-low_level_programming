3805cf530c82f288b9fe5294128d601c456fc296
esteban-silvera <estebansilvera000@gmail.com>
2023-06-28 13:59:58 -0700

    4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - print a numbre whit recurcion
+ *@y: the number
+ *@x: the recurcion
+ *Return: the value
+ */
+int _pow_recursion(int x, int y)
+{
+	int n = y;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
