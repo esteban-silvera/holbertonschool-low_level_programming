db5448a0b8c57c9c6d0ed03e88016aa7bbde57b9
esteban-silvera <estebansilvera000@gmail.com>
2023-06-28 09:22:35 -0700

    3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,25 +2,15 @@
 
 
 /**
- *
- *
+ * factorial - retunr the number factorial give you
+ * @n: the number
  */
 
 int factorial(int n)
 {
-	int x;
-
-	if (n < 0)
+	if (n <= 0)
 	{
 		return (-1);
 	}
-	else if (n >= 0 &&n <= 1)
-	{
-		return (1);
-	}
-	else 
-	{
-		x = n - 1;
-		return (factorial(n * x));
-	}
+	return (n * factorial (n - 1));
 }
