4171606710856f8b7b86e481511fd8850cb90d6d
esteban-silvera <estebansilvera000@gmail.com>
2023-06-28 05:39:46 -0700

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	int x;
+	int n = 0;
+	
+
+	while (n)
+	{
+		n != '\0';
+			n++;
+	}
+	for (x = 0; x != n; x++)
+	{
+		_putchar(s[x]);
+	}
+}
