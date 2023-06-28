d087c36eb7d74ed175d8b6840f6162128f8cf54d
esteban-silvera <estebansilvera000@gmail.com>
2023-06-28 06:41:30 -0700

    2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	int x = 0;
+
+	if (s* != '\0')
+	{
+		x++;
+		x += _strlen_recursion(s + 1);
+	}
+	return (x);
+}
