80513b3da9d104ad63fd7480f130d50351efb278
esteban-silvera <estebansilvera000@gmail.com>
2023-06-29 05:36:09 -0700

    6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+
+int is_prime_helper(int n, int i)
+{
+    if (i <= 1)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+
+	return (is_prime_helper(n, i - 1));
+}
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	return (is_prime_helper(n, n - 1));
+}
