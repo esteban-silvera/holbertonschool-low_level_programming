f1cee3b1efc0a005a4641e871491fcd1b67c651d
esteban-silvera <estebansilvera000@gmail.com>
2023-07-07 05:37:20 -0700

    3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+
+int
+**alloc_grid(int width, int height)
+{
+	int **x;
+	int a, w;
+
+	if (width == '\0')
+	{
+		return ('\0');
+	}
+	if (height == '\0')
+	{
+		return ('\0');
+	}
+	if (width <= 0)
+	{
+		return ('\0');
+	}
+	if (height <= 0)
+	{
+		return ('\0');
+	}
+	
+	x = malloc(sizeof(int*) + height);
+	if (!x)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < height; a++)
+	{
+		x[a] = malloc(sizeof(int) * width);
+
+		if (x[a] == NULL)
+		{
+			for(a >= 0; a--)
+			{
+				free(x[a]);
+			}
+			free(x);
+			return (NULL);
+		}
+	}
+	for (a = 0; a < height; a++)
+	{
+		for (w = 0; w < width; w++)
+		{
+			x[a][w] = 0;
+		}
+	}
+	return (x);
+}
