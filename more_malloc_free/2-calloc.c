4e9ea8a78efabb7f1e3f5d6c5e266ba63ecb6263
esteban-silvera <estebansilvera000@gmail.com>
2023-07-09 07:51:07 -0700

    2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+void
+*_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *string;
+	unsigned int x:
+
+	if (nmemb == 0)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL)
+	}
+
+	string = malloc(nmemb * size);
+	if (!string)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < nmemb * size; x++)
+	{
+		string[x] = 0;
+	}
+}
