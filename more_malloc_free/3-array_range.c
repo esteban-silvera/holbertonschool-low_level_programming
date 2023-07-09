9a0d88c79679c4c5f195eb87683d1f9a0f435d9f
esteban-silvera <estebansilvera000@gmail.com>
2023-07-09 09:25:59 -0700

    3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -5,16 +5,16 @@ int
 *array_range(int min, int max)
 {
 	int x;
-	char *string;
+	int *num;
 
 	string = malloc(sizeof(char*) * max);
 	if (!string)
 	{
 	return (NULL);
 	}
-	for (x = 0; x != max; x++, min++)
+	for (x = 0; min != max; x++, min++)
 	{
-		string[x] = min;
+		num[x] = min;
 	}
-	return (string);
+	return (num);
 }
