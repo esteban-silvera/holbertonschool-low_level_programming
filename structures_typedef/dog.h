26d5eaf35618991b77d943ef1279d16f4e27b73c
esteban-silvera <estebansilvera000@gmail.com>
2023-07-11 14:04:25 -0700

    3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,7 +14,7 @@ dog
 
 	char *owner;
 };
-typedef struct dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
