0488457d59467c0ad7f04ffa528ba402de589ef6
esteban-silvera <estebansilvera000@gmail.com>
2023-07-13 15:08:41 -0700

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,14 +2,14 @@
 void
 array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int x = 0;
+	unsigned int i = 0;
 
 	if (array != NULL && action != NULL && size > 0)
 	{
 		while (x < size)
 		{
-			action(array[x]);
-			x++;
+			action(array[i]);
+			i++;
 		}
 	}
 }
