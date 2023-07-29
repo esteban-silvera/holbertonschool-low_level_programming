f1ee321de23f3fe51d8d7044947936ef736e7787
esteban-silvera <estebansilvera000@gmail.com>
2023-07-29 14:08:01 -0700

    3

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+void
+free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
