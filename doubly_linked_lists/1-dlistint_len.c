8ee24eae7fe0c75ecb3975010eaf55981bb554ad
esteban-silvera <estebansilvera000@gmail.com>
2023-07-28 22:46:55 -0700

    0

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t
+dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
