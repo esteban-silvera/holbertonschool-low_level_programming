8abd175c58d57192cd6b123f108f09036ca665f1
esteban-silvera <estebansilvera000@gmail.com>
2023-07-30 18:43:02 -0700

    1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+void
+print_binary(unsigned long int n) {
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int x = 0;
+
+	while (mask) {
+		if (n & mask)
+		{
+			putchar('1');
+			x = 1;
+		}
+		else if (x)
+		{
+			putchar('0');
+	        }
+
+		mask >>= 1;
+		}
+
+	if (!x) {
+		putchar('0');
+	}
+
+	putchar('\n');
+}
